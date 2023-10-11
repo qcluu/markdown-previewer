ea5aad5198cda1fac5666ea7a81049a160da5db0
qcluu <lcao29@gmail.com>
2023-10-11 01:56:13 -0400

    passed all tests, should be working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { marked } from 'marked';
+import DOMPurify from 'isomorphic-dompurify';
+
+const initialValue = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+
+marked.use({
+  breaks: true
+});
+
+function postprocess(html) {
+  return DOMPurify.sanitize(html);
+}
+
+marked.use({ hooks: { postprocess } });
+
+const Editor = ({ content, handleChange }) => <textarea id = "editor" value={content} onChange={handleChange}></textarea>;
 
 function App() {
+
+  const [markdownText, setMarkdownText] = useState(initialValue);
+  
+  const handleChange = (e) => {
+    setMarkdownText(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor content={markdownText} handleChange={handleChange}/>
+      <h1 id="preview" dangerouslySetInnerHTML={{__html: marked.parse(markdownText)}}></h1>
     </div>
   );
 }
